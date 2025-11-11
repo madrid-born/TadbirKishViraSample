c9143c9cb910a3ddc02aa6a78232f610f78d3a8a
Alireza Noori <alirezanoori.1381@gmail.com>
2025-11-11 23:52:25 +0330

    migrations

---
+++ TKV.Model/Migrations/20251111202109_Initial.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TKV.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coverage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfitCoefficient = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coverage", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequestType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<int>(type: "int", nullable: false),
+                    CoverageId = table.Column<int>(type: "int", nullable: false),
+                    Budget = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RequestType_Coverage_CoverageId",
+                        column: x => x.CoverageId,
+                        principalTable: "Coverage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RequestType_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coverage",
+                columns: new[] { "Id", "ProfitCoefficient", "Title" },
+                values: new object[,]
+                {
+                    { 1, 0.0051999999999999998, "Surgery" },
+                    { 2, 0.0041999999999999997, "Dentistry" },
+                    { 3, 0.0050000000000000001, "Hospitalization" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestType_CoverageId",
+                table: "RequestType",
+                column: "CoverageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestType_RequestId",
+                table: "RequestType",
+                column: "RequestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RequestType");
+
+            migrationBuilder.DropTable(
+                name: "Coverage");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+        }
+    }
+}
